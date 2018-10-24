5ae2e7e84092aee70f588ea739c1c389c412ffdc
mrc <marco.capano@pushapp.me>
2018-10-24 19:38:30 +0200

    Initial commit

---
+++ StaticTableView/StaticTableView.swift
@@ -0,0 +1,79 @@
+//
+//  StaticTableView.swift
+//  StaticTableView
+//
+//  Created by Marco Capano on 24/10/2018.
+//  Copyright © 2018 Marco Capano. All rights reserved.
+//
+
+import UIKit
+
+public class StaticCell: UITableViewCell {
+    public var didSelect: (() -> ())?
+    public var configure: ((StaticCell) -> ())
+    
+    public init(style: UITableViewCell.CellStyle = .default, didSelect: (() -> ())? = nil, configure: @escaping ((StaticCell) -> ())) {
+        self.didSelect = didSelect
+        self.configure = configure
+        super.init(style: style, reuseIdentifier: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+public class Section {
+    public var cells: [StaticCell]
+    public var headerTitle: String?
+    
+    public init(cells: [StaticCell], headerTitle: String?) {
+        self.cells = cells
+        self.headerTitle = headerTitle
+    }
+}
+
+public class StaticTableViewController: UITableViewController {
+    
+    public var sections: [Section] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    public init(sections: [Section], title: String) {
+        self.sections = sections
+        super.init(style: .grouped)
+        navigationItem.title = title
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func cell(for indexPath: IndexPath) -> StaticCell {
+        return sections[indexPath.section].cells[indexPath.row]
+    }
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].cells.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].headerTitle
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.cell(for: indexPath)
+        cell.configure(cell)
+        return cell
+    }
+    
+    override public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        cell(for: indexPath).didSelect?()
+    }
+}

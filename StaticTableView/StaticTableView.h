3d5611d72a80141ac07dce8c57b126d6c5d78e39
mrc <marco.capano@pushapp.me>
2018-10-24 19:33:23 +0200

    Initial Commit

---
+++ StaticTableView/StaticTableView.h
@@ -0,0 +1,19 @@
+//
+//  StaticTableView.h
+//  StaticTableView
+//
+//  Created by Marco Capano on 24/10/2018.
+//  Copyright © 2018 Marco Capano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StaticTableView.
+FOUNDATION_EXPORT double StaticTableViewVersionNumber;
+
+//! Project version string for StaticTableView.
+FOUNDATION_EXPORT const unsigned char StaticTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticTableView/PublicHeader.h>
+
+
